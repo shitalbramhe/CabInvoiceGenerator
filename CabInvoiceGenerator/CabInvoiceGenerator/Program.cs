fa195274711c55e9963aca7ab9e489d5c565c7bc
shital bramhe <shitalbramhe@gmail.com>
2021-12-10 13:35:02 +0530

    [shital] ADD: initial commit

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Cab Invoice Generator");
+        }
+    }
+}
